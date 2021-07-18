29709dd4eed62e720f061a2440cb1e2cc61ef926
biolyric <41186531@qq.com>
2021-07-18 14:20:41 +0800

    commit on master branch

--- src/main/java/com/javaFXGUI/MiceVBox.java
+++ src/main/java/com/javaFXGUI/MiceVBox.java
@@ -994,7 +994,6 @@ public class MiceVBox extends VBox {
                 clearNotesButton=new Button("Clear"),
                 updateButton=new Button("Update Mouse");
 
-
         public void setTagNumberTextField(String s){
             tagNumberTextField.setText(s);
         }
